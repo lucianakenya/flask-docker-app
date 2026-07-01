1b98fe1a383a055e9272c66a85207f793e7ded39
Luciana Kenya <nyamburalucy678@gmail.com>
2026-07-01 09:34:22 +0300

    Initial commit - Flask Docker app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Hello from my container!", "status": "running"})
+
+@app.route("/health")
+def health():
+    return jsonify({"health": "ok"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
